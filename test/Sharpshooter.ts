dba2335f0453a120e953efb647913ebe8dc0f253
cherrypopsicle <majd.khoury93@gmail.com>
2024-02-20 12:12:06 +0000

    little bit of a clean up.

--- test/Sharpshooter.ts
+++ test/Sharpshooter.ts
@@ -1,17 +1,18 @@
 import {expect} from "chai";
 import {ethers} from "hardhat";
 import {Contract, Signer} from "ethers";
-import {SharpshooterPass} from "../typechain/contracts/SharpshooterPass"; // Adjust the import path to where your typechain artifacts are located
+import {SharpshooterPass} from "../typechain/contracts/SharpshooterPass";
+import {token} from "../typechain/@openzeppelin/contracts"; // Adjust the import path to where your typechain artifacts are located
 
 describe("SharpshooterPass", function () {
     let sharpshooterPass: SharpshooterPass;
     let owner: Signer;
-    let addr1: JSON;
+    let addr1: Signer;
 
     beforeEach(async function () {
         [owner, addr1] = await ethers.getSigners();
         const SharpshooterPass = await ethers.getContractFactory("SharpshooterPass");
-        sharpshooterPass = SharpshooterPass.attach("0xCA87833e830652C2ab07E1e03eBa4F2c246D3b58");
+        sharpshooterPass = SharpshooterPass.attach("0x516f93CF42231a157F8baBc07E852ED727Cd5FFA");
     });
 
     it("Should mint a new token if the signature is valid", async function () {
@@ -27,6 +28,10 @@ describe("SharpshooterPass", function () {
         // Ensure the proof string is correctly prefixed with '0x'
         const isValidProof = await sharpshooterPass.verifyNFTProof(tokenId, correctProof);
         expect(isValidProof).to.be.true;
-        // expect(balance).to.equal(amount);
+
+
+        await sharpshooterPass.mintNFT(tokenId, correctProof);
+        const balance = await sharpshooterPass.balanceOf(await owner.getAddress(), tokenId);
+        expect(balance).to.equal(amount);
     });
 });
